e3b41cf5eca1baeb09940dcf888e7b30a1448eac
tokoyax <tokoyax.dev@gmail.com>
2018-05-17 18:00:17 +0900

    chapter 5.8.2 done
    モナド変換子 途中

---
+++ src/monad-transformers.hs
@@ -0,0 +1,19 @@
+import           Control.Monad.Trans.Class  (lift)
+import           Control.Monad.Trans.Reader (ReaderT, asks)
+
+data Env = Env { envX :: !Integer, envY :: !Integer }
+
+sumEnv :: ReaderT Env IO Integer
+sumEnv = do
+    x <- asks envX
+    y <- asks envY
+    return (x + y)
+
+sumEnvIO :: ReaderT Env IO Integer
+sumEnvIO = do
+    x <- asks envX
+    lift $ putStrLn ("x=" ++ show x)
+    y <- asks envY
+    lift $ putStrLn ("y=" ++ show y)
+    return (x + y)
+
