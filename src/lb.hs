0864e2da92921f048c9f75596d30e3baf5cbacfd
tokoyax <tokoyax.dev@gmail.com>
2018-05-18 17:57:09 +0900

    Chapter 5 done
    モナド変換子おわり
    復習すること

---
+++ src/lb.hs
@@ -0,0 +1,19 @@
+import           Control.Exception.Lifted (bracket)
+import           Control.Monad.Reader     (ask, runReaderT)
+import           Control.Monad.Trans      (lift)
+import           System.IO                (IOMode (ReadMode), hClose,
+                                           hGetContents, openFile)
+
+path :: FilePath
+path = "src/chap05/src/lifted-bracket.hs" -- ファイルを保存しておく
+
+main :: IO ()
+main = (`runReaderT` path) $ do
+    bracket open close $ \h -> do
+        content <- lift (hGetContents h)
+        lift $ print (length content)
+    where
+        open = do
+            p <- ask
+            lift $ openFile p ReadMode
+        close = lift . hClose
