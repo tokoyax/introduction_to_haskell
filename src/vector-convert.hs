40547975e773d98ec3add02e334cb0c88989273e
tokoyax <tokoyax.dev@gmail.com>
2018-05-23 13:35:32 +0900

    chapter 7.4 done
    Vector おわり

---
+++ src/vector-convert.hs
@@ -0,0 +1,12 @@
+import           Control.Monad
+import qualified Data.Vector         as V
+import qualified Data.Vector.Mutable as VM
+
+main :: IO ()
+main = do
+    as <- V.thaw animals
+    VM.write as 3 "Wolf" -- Fox を Wolf に書き換える
+    print =<< V.freeze as
+
+animals :: V.Vector String
+animals = V.fromList ["Dog", "Pig", "Cat", "Fox", "Mouse", "Cow", "Horse"]
